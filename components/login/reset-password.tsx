15f6b033028bbaab501667f6c1396afdd73ce366
devVitoria <vitoria.aparecida.dsantos@gmail.com>
2026-01-12 23:59:38 -0300

    initial reset-passoword template cc-1-t

---
+++ components/login/reset-password.tsx
@@ -0,0 +1,13 @@
+import { Modal, View } from "react-native";
+
+export default function ResetPassword() {
+  return (
+    <Modal visible transparent>
+      <View className="bg-black/20 items-center justify-center flex-1 px-8">
+        <View className="w-full h-1/2 bg-white rounded-lg justify-center items-center">
+        
+        </View>
+      </View>
+    </Modal>
+  );
+}
