6fb37f8cba8fef762a871b79c0e4bcfc4ed3d8e0
devVitoria <vitoria.aparecida.dsantos@gmail.com>
2026-01-30 23:04:03 -0300

    adding loading chat animation

--- app/financial-chat.tsx
+++ app/financial-chat.tsx
@@ -14,6 +14,7 @@ import {
   View,
 } from "react-native";
 import ChatBackground from "../assets/images/chatBackground.svg";
+import LottieView from "lottie-react-native";
 
 export default function FinancialChat() {
   const { user } = useUserStore();
@@ -71,55 +72,65 @@ export default function FinancialChat() {
             </View>
             <SimpleLineIcons name="options-vertical" size={20} color="white" />
           </View>
+          {isLoading ? (
+            <View className="flex-1 justify-center items-center z-50">
+              <LottieView
+                source={require("../assets/json/loading.json")}
+                autoPlay
+                loop
+                style={{ width: 400, height: 400 }}
+              />
+            </View>
+          ) : (
+            <FlatList
+              className="flex-1 w-full z-50 "
+              data={sentMessages}
+              keyExtractor={(_, index) => index.toString()}
+              showsVerticalScrollIndicator={false}
+              renderItem={(item) => {
+                const showDay =
+                  item.index === 0 ||
+                  sentMessages?.[item.index - 1]?.created_at.split(" ")[0] !==
+                    item.item.created_at.split(" ")[0];
 
-          <FlatList
-            className="flex-1 w-full z-50 "
-            data={sentMessages}
-            keyExtractor={(_, index) => index.toString()}
-            showsVerticalScrollIndicator={false}
-            renderItem={(item) => {
-              const showDay =
-                item.index === 0 ||
-                sentMessages?.[item.index - 1]?.created_at.split(" ")[0] !==
-                  item.item.created_at.split(" ")[0];
-
-              const isCurrentDate =
-                item.item.created_at.split(" ")[0]?.replaceAll("/", "-") ===
-                dayjs().format("DD-MM-YYYY");
-              return (
-                <View className="py-2 justify-end items-end">
-                  {showDay && (
-                    <View className="flex w-full items-center justify-center py-2 pb-8">
-                      <View
-                        className={cn(
-                          `w-42 px-2 py-1 rounded-md bg-[#854d0e20]`,
-                          { isCurrentDate: "w-24" },
-                        )}
-                      >
-                        <Text className="text-center text-acessiPrimary font-bold">
-                          {isCurrentDate
-                            ? "Hoje"
-                            : item.item.created_at.split(" ")[0]}
-                        </Text>
+                const isCurrentDate =
+                  item.item.created_at.split(" ")[0]?.replaceAll("/", "-") ===
+                  dayjs().format("DD-MM-YYYY");
+                return (
+                  <View className="py-2 justify-end items-end">
+                    {showDay && (
+                      <View className="flex w-full items-center justify-center py-2 pb-8">
+                        <View
+                          className={cn(
+                            `w-42 px-2 py-1 rounded-md bg-[#854d0e20]`,
+                            { isCurrentDate: "w-24" },
+                          )}
+                        >
+                          <Text className="text-center text-acessiPrimary font-bold">
+                            {isCurrentDate
+                              ? "Hoje"
+                              : item.item.created_at.split(" ")[0]}
+                          </Text>
+                        </View>
                       </View>
-                    </View>
-                  )}
+                    )}
 
-                  <View className="w-[80%] border-[#854d0e33] bg-[#854d0e20] border-[0.5px] py-2 rounded-md p-2">
-                    <Text className="text-acessiPrimary">
-                      {item.item.reason}
-                    </Text>
+                    <View className="w-[80%] border-[#854d0e33] bg-[#854d0e20] border-[0.5px] py-2 rounded-md p-2">
+                      <Text className="text-acessiPrimary">
+                        {item.item.reason}
+                      </Text>
+                    </View>
+                    <View className="w-14 h-5  bg-[#854d0e20] flex flex-row  mt-2 rounded-md items-center justify-around">
+                      <FontAwesome5 name="clock" size={10} color="#854d0e" />
+                      <Text className="text-xs text-acessiPrimary">
+                        {item.item.created_at.split(" ")[1]}
+                      </Text>
+                    </View>
                   </View>
-                  <View className="w-14 h-5  bg-[#854d0e20] flex flex-row  mt-2 rounded-md items-center justify-around">
-                    <FontAwesome5 name="clock" size={10} color="#854d0e" />
-                    <Text className="text-xs text-acessiPrimary">
-                      {item.item.created_at.split(" ")[1]}
-                    </Text>
-                  </View>
-                </View>
-              );
-            }}
-          />
+                );
+              }}
+            />
+          )}
           <View className="flex flex-row h-16 w-full bg-[#854d0e77] justify-between items-center px-4 rounded-xl my-4 ">
             <TextInput
               value={message}
